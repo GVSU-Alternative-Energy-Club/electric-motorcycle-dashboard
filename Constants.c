53b849bd430ebd286ab2bf922fbfd050fdf29451
smutnyj <smutnyj@mail.gvsu.edu>
2016-10-03 19:44:29 -0400

    Add files via upload
    Constants.c ... Created file to house all micros for x-y coordinates to display all necessary numbers on the LCD such as Speed, RPM, etc
    
    main.c ... Modified file with two for loops in main() in order to select images needed for Speed's digits and set cursor for Speed

---
+++ Constants.c
@@ -0,0 +1,52 @@
+/*
+Description: Code to house all of the necessary constants affected by the code
+*/
+
+#include "macros.h"
+#include "disp_driver.h"
+
+#include "cat.h"
+
+//speed display constants for cursor locations
+#define SPEED_DISPLAY_DIGIT_X
+#define SPEED_DISPLAY_DIGIT_SPACING
+#define SPEED_DISPLAY_DIGIT_Y
+
+//RPM display constants for cursor locations
+#define RPM_DISPLAY_DIGIT_X
+#define RPM_DISPLAY_DIGIT_SPACING
+#define RPM_DISPLAY_DIGIT_Y
+
+//Odometer display constants for cursor locations
+#define DIST_DISPLAY_DIGIT_X
+#define DIST_DISPLAY_DIGIT_SPACING
+#define DIST_DISPLAY_DIGIT_Y
+
+//Turn Signals warning display constants for cursor locations
+#define LEFT_TURN_DISPLAY_X
+#define RIGHT_TURN_DISPLAY_X
+#define TURN_DISPLAY_Y
+
+//High-Beam warning display constants for cursor locations
+#define HIGH_BEAM_DISPLAY_X
+#define HIGH_BEAR_DISPLAY_Y
+
+//Low-Beam warning display constants for cursor locations
+#define LOW_BEAM_DISPLAY_X
+#define LOW_BEAM_DISPLAY_Y
+
+//Low Battery warning display constants for cursor locations
+#define LOW_BATTERY_DISPLAY_X
+#define LOW_BATTERY_DISPLAY_Y
+
+//Improper Torque warning (too much or too little) display constants for cursor locations
+#define TORQUE_DISPLAY_X
+#define TORQUE_DISPLAY_Y
+
+//All numbers displayed's width and height on screen
+#define NUMBER_DISPLAY_IMG_WIDTH
+#define NUMBER_DISPLAY_iMG_HEIGHT
+
+void main()
+{
+}
